ba463bfdd62ac387f6c9b450982fb8cba1635323
Nick Eikenberg <nickeikenberg95@gmail.com>
2021-11-29 18:05:36 -0500

    Data renders on airline show page

--- app/javascript/components/Airlines/Airlines.js
+++ app/javascript/components/Airlines/Airlines.js
@@ -1,19 +1,42 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import Airline from "./Airline";
+import styled from "styled-components";
+
+const Home = styled.div`
+  text-align: center;
+  max-width: 1200px;
+  margin: 0 auto;
+`;
+const Header = styled.div`
+  padding: 100px 100px 10px 100px;
+
+  h1 {
+    font-size: 42px;
+  }
+`;
+const Subheader = styled.div`
+  font-weight: 300;
+  font-size: 26px;
+`;
+const Grid = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  width: 100%;
+  padding: 2em;
+
+  ul {
+    width: 100%;
+  }
+`;
 
 const Airlines = () => {
   const [airlines, setAirlines] = useState([]);
 
   useEffect(() => {
-    // Get all of our airlines from api
-    // Update airlines in our state
-
     axios
       .get("/api/v1/airlines.json")
-      .then((res) => {
-        setAirlines(res.data.data);
-      })
+      .then((res) => setAirlines(res.data.data))
       .catch((res) => console.log(res));
   }, []);
 
@@ -27,15 +50,15 @@ const Airlines = () => {
   });
 
   return (
-    <div className="home">
-      <div className="header">
+    <Home>
+      <Header>
         <h1>OpenFlights</h1>
-        <div className="subheader">Honest, unbiased airline reviews</div>
-      </div>
-      <div className="grid">
+        <Subheader>Honest, unbiased airline reviews</Subheader>
+      </Header>
+      <Grid>
         <ul>{grid}</ul>
-      </div>
-    </div>
+      </Grid>
+    </Home>
   );
 };
 
